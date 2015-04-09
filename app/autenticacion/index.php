0cbe1e1a2c39a704d5e79ce5ead58b0241cf3611
yvan <mayvan@hotmail.com>
2015-04-09 16:11:16 -0400

    yvan> actualizacion del menu

--- app/autenticacion/index.php
+++ app/autenticacion/index.php
@@ -17,51 +17,55 @@
         <link href="css/bootstrap.min.css" rel="stylesheet"/>
         <link href="css/custom.css" rel="stylesheet"/>
         <link href="css/toaster.css" rel="stylesheet"/>
-        <style>
+        <!--<style>
           a {
           color: orange;
           }
-        </style>
+        </style>-->
     
   </head>
 
   <body ng-cloak="">
     <div ng-if='uid && name'>        
-      <div class="navbar navbar-inverse navbar-fixed-top navbar-default megamenu" role="navigation">
-      <div class="container" ng-controller="NavCtrl">
-        <div class="col-sm-12">          
-            <div class="navbar-header">
-            <BR><font size="3" color="white">USUARIO: {{name}}</font>
-            <button type="button" data-toggle="collapse" data-target="#navbar-collapse-grid" class="navbar-toggle">
+      <div class="navbar navbar-fixed-top navbar-default megamenu" >
+      <nav class="navbar navbar-default">
+      <div class="container-fluid" >
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span class="sr-only">Ini</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
-            </button> <br>                
+            </button>
+            <a class="navbar-brand" href="#">*</a>
           </div>
-          <div id="navbar-collapse-grid" class="navbar-collapse collapse" ng-controller="NavCtrl">
+          <div class="navbar-collapse collapse"  id="bs-example-navbar-collapse-1"ng-controller="NavCtrl">
             <ul class="nav navbar-nav">
-            <li ng-repeat ="p in categories" class="dropdown megamenu-fw"><a href="#" data-toggle="dropdown" class="dropdown-toggle">{{p.grupo}}<b class="caret"></b></a>
-              <ul class="dropdown-menu">
-              <li class="grid-demo">
-                <div class="row">
-                <div class="col-sm-3" ng-repeat="c in p.sub_categories">
-                  <!--<a href="#/angular/{{c.id_grupo}}/{{c.description}}">{{c.opcion}}-->
+              <li class="active">
+                <a href="#">INICIO <span class="sr-only">(current)</span></a>
+              </li>
+            <li ng-repeat ="p in categories" class="dropdown">
+            <a href="#" data-toggle="dropdown" class="dropdown-toggle">{{p.grupo}}
+            <span class="caret"></span></a>
+              <ul class="dropdown-menu"  role="menu">
+              <li ng-repeat="c in p.sub_categories">
                   <a href="#{{c.id_opcion}}" id="{{c.contenido}}">{{c.opcion}}</a>
-                </div>                   
-                </div>
               </li>
               </ul>
             </li>
             </ul>
-          </div>             
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">USUARIO: {{name}}</a></li>
+              <li><a href="#administracion|usuarios">opcion ejemplo</a></li>
+              <li><a href="#">SALIR</a></li>
+              <li><a ng-click="logout();" >SALIR</a></li>
+            </ul>
+          </div>
         </div>
+        </nav>
       </div>
     </div>
-    </div> 
-
-	  <a href="#administracion|usuarios">opcion ejemplo</a>
-	
-    <footer class="footer">
+   <foter class="footer">
       <div >
         <div class="container" style="margin-top:20px;">
           <div data-ng-view="" id="ng-view" class="slide-animation"></div>
@@ -75,6 +79,32 @@
   <script src="js/angular-animate.min.js" ></script>
   <script src="js/toaster.js"></script>
   <script src="js/jquery-1.11.2.min.js"></script>
+ <!--  nuevas librearias adicionales-->
+
+<link rel="stylesheet" href="../../libs/components/bootstrap/dist/css/bootstrap.min.css" />
+    <!-- endbower -->
+    <!-- build:css(.tmp) styles/main.css -->
+    <link rel="stylesheet" href="../../libs/styles/main.css">
+    <link rel="stylesheet" href="../../libs/styles/sb-admin-2.css">
+    <link rel="stylesheet" href="../../libs/styles/timeline.css">
+    <link rel="stylesheet" href="../../libs/components/metisMenu/dist/metisMenu.min.css">
+    <link rel="stylesheet" href="../../libs/components/angular-loading-bar/build/loading-bar.min.css">
+    <link rel="stylesheet" href="../../libs/components/font-awesome/css/font-awesome.min.css" type="text/css">
+    <!-- endbuild -->
+    
+    <!-- build:js(.) scripts/vendor.js -->
+    <!-- bower:js -->
+    <script src="../../libs/components/jquery/dist/jquery.min.js"></script>
+    <script src="../../libs/components/angular/angular.min.js"></script>
+    <script src="../../libs/components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../../libs/components/angular-ui-router/release/angular-ui-router.min.js"></script>
+    <script src="../../libs/components/json3/lib/json3.min.js"></script>
+    <script src="../../libs/components/oclazyload/dist/ocLazyLoad.min.js"></script>
+    <script src="../../libs/components/angular-loading-bar/build/loading-bar.min.js"></script>
+    <script src="../../libs/components/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>
+    <script src="../../libs/components/metisMenu/dist/metisMenu.min.js"></script>
+    <!-- endbower -->
+  <!-- app -->
   <script src="app/app.js"></script>
   <script src="app/data.js"></script>
   <script src="app/directives.js"></script>
