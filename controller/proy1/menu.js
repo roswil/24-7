c7792f7fb1ba9462f56dfcf77af47f4ba0eb8763
juanVidalTroche <trocov.01@gmail.com>
2015-04-10 18:37:12 -0400

    Limpieza

--- controller/proy1/menu.js
+++
@@ -1,7 +0,0 @@
-app = angular.module('app', []);
-app.controller("NavCtrl",function($scope,$http){
-    var serviceBase = '../../model/proy2_grillas_restful/cliente.php/';
-    $http.get(serviceBase + 'categories').then(function (results) {
-        $scope.categories = results.data;
-    });
-});
